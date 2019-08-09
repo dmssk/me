a67933b0f21b442b716430317e5f1978755ce78f
Dima Puzyrenko <myaczt@gmail.com>
2019-08-09 17:51:49 +0300

    update(Works.vue)

--- root-path/src/App.vue
+++ root-path/src/App.vue
@@ -77,8 +77,14 @@
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
+    width: 100%;
+    height: 100%;
 
     nav {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
       text-align: center;
       background: #d1f2f3;
       padding: 15px 0;
